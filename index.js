672bdae505dd901208d61dad959a824c60e016e4
niiyazobug <niiyaz5555@gmail.com>
2022-04-24 03:04:34 +0600

    get all priducts method added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const port = process.env.PORT || 5000
 
@@ -15,23 +16,31 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.EMAJHON_USER}:${process.env.EMAJHON_PASS}@cluster0.vbiac.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const run = async()=>{
-try{
+const run = async() => {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJhon').collection('product');
 
-}
-finally{
-    
-}
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        })
+    }
+    finally {
+        // await client.close();
+    }
 }
 
 run().catch(console.dir)
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Ema-Jhon server')
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Ema-Jhon server is runnning', port);
 })
